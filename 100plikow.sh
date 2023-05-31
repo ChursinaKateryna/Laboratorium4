df03f4caa56cace12a01584e1d511fba6da2ec0e
ChursinaKateryna <chursina.katarzyna@gmail.com>
2023-05-31 13:55:08 +0200

    Tworzy 100 plików

---
+++ 100plikow.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$1" = "--logs" ]; then
+   for ((i=1; i<=100; i++)); do 
+      filename="log${i}.txt"
+      echo "Nazwa pliku: $filename" > $filename
+      echo "Nazwa skryptu: $0" >> $filename
+      echo "Data: $(date)" >> $filename
+   done
+fi
